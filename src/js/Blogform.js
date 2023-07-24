27ee633070d938568ec2573ce19128fc454f8629
zhuang9197 <2280779141@qq.com>
2023-07-25 01:18:04 +0800

    [update]

--- src/js/Blogform.js
+++ src/js/Blogform.js
@@ -1,28 +1,104 @@
-const BlogDom = document.getElementById('page');
-const HtmlStyle = {"Text":"<P>","TextEnd":"</P>","":""}
 
-
-function Blogform(url)
-{
-    fetch(url)
-    .then(function (response){
+function Blog(Id){
+    fetch("https://cdn.jsdelivr.net/gh/zhuang9197/Resource-Silo/Json/Blog/Blog_Database.json")
+    .then(function(response){
         if(response.ok){
             return response.json();
         }
         throw new Error('Network response was not ok');
     })
     .then(function(json){
-        var html
-        for(i=0;i<json.Blog.length;i++){
-            html+=HtmlStyle[json.Blog[i]["head"]];
-            html += json.Blog[i]["body"];
-            html += HtmlStyle[json.Blog[i]["foot"]];
+        debugger
+        var html_li = "";
+        BlogJson = json.List.find(item => item.Id == Id);
+        document.getElementById('blog-Title').textContent = BlogJson.Title;
+        document.getElementById('blog-pubication').textContent = BlogJson.pubication;
+        document.getElementById('blog-update').textContent = BlogJson.update;
+        if(BlogJson.Nav.length >=1)
+        {
+            for(i=0;i<=BlogJson.Nav.length;i++){
+                html_li += `<li><a class = "a-noDecoration" href="${BlogJson.Nav[i]["Id"]}">${BlogJson.Nav[i]["Title"]}</a></li>`;
+            }
         }
-        BlogDom.innerHTML += html;
-    })
-    .catch(function (error){
-        console.log('Error:',error.message);
-    });
+        if(Id<=1){
+            document.getElementById('Button_Last').style.display = 'none';
+        }else{
+            var Last = document.getElementById('Button_Last');
+            Last.onclick = function(){
+                Blogform(json.List.find(item => item.Id == (Id-1)).Url);
+                Blog(Id-1);
+            }
+            Last.title = json.List.find(item => item.Id == (Id-1)).Title;
+            
+
+        }
+
+        if(Id>=json.List.length){
+            document.getElementById('Button_Next').style.display = 'none';
+        }else{
+            var Next = document.getElementById('Button_Next');
+            Next.onclick = function(){
+                Blogform(json.List.find(item => item.Id == (Id+1).Url));
+                Blog(Id+1);
+            }
+            Next.title = json.List.find(item => item.Id == (Id+1)).Title;
+        }
+
+        })
+}
+
+function Blogform(url) {
+    
+    fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+                return response.text();
+            }
+            throw new Error('Network response was not ok');
+        })
+        .then(function (text) {
+         
+            
+            var html = text
+                .replace(/#\d/g, function (match) {
+                    var id = match.substring(1);
+                    return '<h' + id + '>';
+                })
+                .replace(/#d/g, '<div>')
+                .replace(/#p/g, '<p>')
+                .replace(/#a-(\d+)/g, '<a id="section$1">')
+                .replace(/#r/g, '<pre>')
+                .replace(/#c/g, '<code>')
+                .replace(/@d/g, '</div>')
+                .replace(/@\d/g, function (match) {
+                    var id = match.substring(1);
+                    return '</h' + id + '>';
+                })
+                .replace(/@p/g, '</p>')
+                .replace(/@a/g, '</a>')
+                .replace(/@r/g, '</pre>')
+                .replace(/@c/g, '</code>')
+                .replace(/\$1/g, '<h1 class="Directory">')
+                .replace(/\$d-o/g, 'div class="Output">')
+                .replace(/\$c-cs/g, '<code class="language-csharp box-shadow">')
+                .replace(/\$c-sql/g, '<code class="language-sql box-shadow">')
+                .replace(/%d/g, '<div id="cnblogs_post_body" class="blogpost-body cnblogs-markdown">')
+                .replace(/#!/g, '<!--')
+                .replace(/@!/g, '--!>');
+
+            BlogDom = document.getElementById('BlogTitle');
+            BlogDom.innerHTML = html;
+            // 手动初始化Highlight.js
+            hljs.highlightAll();
+            // 添加行号
+            document.querySelectorAll("code.hljs").forEach(function (block) {
+                hljs.lineNumbersBlock(block);
+            });
+
+        })
+        .catch(function (error) {
+            console.log('Error:', error.message);
+        });
 }
 
 Element.prototype.Blogform = Blogform;
--- src/js/listform.js
+++ src/js/listform.js
@@ -28,10 +28,10 @@
                 for(i=index.listIndex;i<num;i++){
                     var html = `<li class="post-list-item fade in" style="background-image: url(https://cdn.jsdelivr.net/gh/zhuang9197/Resource-Silo/Image/Blog/Database/DatabaseLine.png)">`;
                         html += `<article id = "${json.List[i]["id"]}" class = "article-card article-type-post animation-show" style = "visibility:hidden;">`;
-                        html += `<h3 class ="post-title"><a href = "${json.List[i]['Url']}">"${json.List[i]['Title']}"</a></h3>`;
+                        html += `<h3 class ="post-title"><a href = "../Blog_Title.html?Url=${json.List[i]["Url"]}&Id=${json.List[i]["Id"]}" >"${json.List[i]['Title']}"</a></h3>`;
                         html += `<div class ="post-meta"><time itemprop="dateCreated datePublished" class = "post-time"></tiem></div>`;
-                        html += `<div class ="post-content" style = "color:#666"><p>${json.List[i]['Url']}</p><div><a href = "${json.List[i]['Url']}" class="post-more waves-button alone">阅读全文<span>>></span></a></div></div>`;
-                        html += `<div class = "post-footer"><ul class="article-tag-list" itemprop="keywords"><li class="article-tag-list-item"><a class="article-tag-list-link" href="${json.List[i]['Url']}" rel = "tag"></a></li><li class="article-tag-list-item"><a class='article-tag-list-link" href="${json.List[i]['Url']}"></a></li></ul></div>`
+                        html += `<div class ="post-content" style = "color:#666"><p>${json.List[i]['Url']}</p><div><a href = "../Blog_Title.html?Url=${json.List[i]["Url"]}&Id=${json.List[i]["Id"]}" class="post-more waves-button alone">阅读全文<span>>></span></a></div></div>`;
+                        html += `<div class = "post-footer"><ul class="article-tag-list" itemprop="keywords"><li class="article-tag-list-item"><a class="article-tag-list-link" href="../Blog_Title.html?Url=${json.List[i]["Url"]}&Id=${json.List[i]["Id"]}"}" rel = "tag"></a></li><li class="article-tag-list-item"><a class='article-tag-list-link" href="../Blog_Title.html?Url=${json.List[i]["Url"]}&Id=${json.List[i]["Id"]}"></a></li></ul></div>`
                         html +=`</article>`;
                         html +=`</li>`;
                     
